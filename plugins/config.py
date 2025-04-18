c587e0814d10225b89c6566b1942977714d06488
sameheresakib <sameheresakib@gmail.com>
2025-04-18 15:09:18 +0300

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -33,11 +33,11 @@ class Config(object):
 
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://uploaderskbot:<himu1122>@cluster0.sbtvozx.mongodb.net/?retryWrites=true&w=majority")
 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002573938501"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1002573938501"))
     LOGGER = logging
     OWNER_ID = int(os.environ.get("OWNER_ID", "1700797877"))
     SESSION_NAME = "Uploaderskbot"
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002573938501"))
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "1002573938501")
 
     TG_MIN_FILE_SIZE = 2194304000
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "moviesbd_bot")
